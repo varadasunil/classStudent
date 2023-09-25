a30b7f6aba2a718f63f67bd561c841aba66882d5
Varada <Varadavalsalya@gmail.com>
2023-09-25 17:34:22 +0530

    initial commit

---
+++ classStudent/src/main/java/com/trials/classStudent/ClassStudentApplication.java
@@ -0,0 +1,13 @@
+package com.trials.classStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassStudentApplication.class, args);
+	}
+
+}
---
+++ classStudent/src/test/java/com/trials/classStudent/ClassStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trials.classStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
